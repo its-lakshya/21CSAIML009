34448b0bc3b59fc651025ce14d02b771a11cd7f7
Lakshya Kumar <kumarlakshya101@gmail.com>
2024-07-20 12:33:07 +0530

    feat: complete the average calculator

--- averageCalculator/src/index.js
+++ averageCalculator/src/index.js
@@ -5,52 +5,125 @@ import axios from "axios";
 
 export { app };
 
+
+// Defining constants
 const PORT = 9876;
 const WINDOW_SIZE = 10;
+const AUTH_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzIxNDU5MDQ4LCJpYXQiOjE3MjE0NTg3NDgsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImRmOWFjMGFlLTE3NzMtNDkyYS05MGU5LTUxNjZlODE2OTc4YyIsInN1YiI6IjIxY3NhaW1sMDA5QGpzc2F0ZW4uYWMuaW4ifSwiY29tcGFueU5hbWUiOiJJbnN0YU1hcnQiLCJjbGllbnRJRCI6ImRmOWFjMGFlLTE3NzMtNDkyYS05MGU5LTUxNjZlODE2OTc4YyIsImNsaWVudFNlY3JldCI6IlRSR0hJdUpoU0JYblpUVFIiLCJvd25lck5hbWUiOiJMYWtzaHlhIiwib3duZXJFbWFpbCI6IjIxY3NhaW1sMDA5QGpzc2F0ZW4uYWMuaW4iLCJyb2xsTm8iOiIyMUNTQUlNTDAwOSJ9.DT9nWpmjft7qGxpq1o2HCMWDt54FFYPzCuvu6FghNBw';
+const NUMBERS_URL = 'http://20.244.56.144/test/';
 
+
+// Configuring axios
 const axiosConfig = {
   headers: {
-    Authorization: `Bearer ${process.env.authToken}`,
+    Authorization: `Bearer ${AUTH_TOKEN}`,
   },
 };
 
+// Listening server
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
 });
 
-function calculateAverage(numbers) {
-  if (numbers.length === 0) {
-    return 0; // Handling edge case: empty array
+// Calculation of averate 
+function calculateAverage(arr) {
+  if (arr.length === 0) {
+      return 0; // To handle empty array scenario
   }
 
-  let sum = numbers.reduce((acc, num) => acc + num, 0);
-  let average = sum / numbers.length;
-  return average;
+  let sum = 0;
+  for (let num of arr) {
+      sum += num;
+  }
+  return sum / arr.length;
 }
 
 // For prime numbers
 app.get("/numbers/p", (_, res) => {
   let primeNumbers;
-  let numberWindowSize;
+  let numberWindowSize = primeNumbers;
   axios
-    .get(`${process.env.NUMBERS_URL}` + "primes", axiosConfig)
+    .get(`${NUMBERS_URL}` + "primes", axiosConfig)
     .then((response) => {
       primeNumbers = response.data.numbers;
-      console.log("Response:", response.data);
-      if (primeNumbers.length() > WINDOW_SIZE) {
-        difference = WINDOW_SIZE - primeNumbers.length;
-        numberWindowSize = primeNumbers.splice(0, n);
-      }
+      numberWindowSize = [...primeNumbers];
+
+      if (primeNumbers.length > WINDOW_SIZE) {
+        const difference = WINDOW_SIZE - primeNumbers.length;
+        numberWindowSize.splice(0, difference);
+        }
+
     
-      res.send(
-        200,
-        json({
+      res.status(200)
+      .send(
+        {
           windowPrevState: [],
           windowCurrentState: numberWindowSize,
           numbers: primeNumbers,
           avg: calculateAverage(numberWindowSize),
-        })
-      );
+        }
+      )
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+});
+
+// For fibonacci numbers
+app.get("/numbers/f", (_, res) => {
+  let fiboNumbers;
+  let numberWindowSize;
+  axios
+    .get(`${NUMBERS_URL}` + "fibo", axiosConfig)
+    .then((response) => {
+      fiboNumbers = response.data.numbers;
+      numberWindowSize = [...fiboNumbers];
+
+      if (fiboNumbers.length > WINDOW_SIZE) {
+        const difference = WINDOW_SIZE - fiboNumbers.length;
+        numberWindowSize.splice(0, difference);
+      }
+    
+      res.status(200)
+      .send(
+        {
+          windowPrevState: [],
+          windowCurrentState: numberWindowSize,
+          numbers: fiboNumbers,
+          avg: calculateAverage(numberWindowSize),
+        }
+      )
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
+});
+
+// For even numbers
+app.get("/numbers/e", (_, res) => {
+  let evenNumbers;
+  let numberWindowSize;
+  axios
+    .get(`${NUMBERS_URL}` + "even", axiosConfig)
+    .then((response) => {
+      evenNumbers = response.data.numbers;
+      numberWindowSize = [...evenNumbers];
+
+      if (evenNumbers.length > WINDOW_SIZE) {
+        const difference = WINDOW_SIZE - evenNumbers.length;
+        numberWindowSize.splice(0, difference);
+      }
+    
+      res.status(200)
+      .send(
+        {
+          windowPrevState: [],
+          windowCurrentState: numberWindowSize,
+          numbers: evenNumbers,
+          avg: calculateAverage(numberWindowSize),
+        }
+      )
     })
     .catch((error) => {
       console.error("Error:", error);
@@ -59,93 +132,33 @@ app.get("/numbers/p", (_, res) => {
   
 });
 
-// For fibonacci numbers
-
-app.get("/numbers/f", (_, res) => {
-  let fiboNumbers;
-  let numberWindowSize;
-  axios
-    .get(`${process.env.NUMBERS_URL}` + "fibo", axiosConfig)
-    .then((response) => {
-      fiboNumbers = response.data.numbers;
-      console.log("Response:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-
-  if (fiboNumbers.length > WINDOW_SIZE) {
-    difference = WINDOW_SIZE - fiboNumbers.length;
-    numberWindowSize = fiboNumbers.splice(0, n);
-  }
-
-  res.send(
-    200,
-    json({
-      windowPrevState: [],
-      windowCurrentState: numberWindowSize,
-      numbers: fiboNumbers,
-      avg: calculateAverage(numberWindowSize),
-    })
-  );
-});
-
-// For even numbers
-app.get("/numbers/e", (_, res) => {
-  let evenNumbers;
-  let numberWindowSize;
-  axios
-    .get(`${process.env.NUMBERS_URL}` + "even", axiosConfig)
-    .then((response) => {
-      evenNumbers = response.data.numbers;
-      console.log("Response:", response.data);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-
-  if (evenNumbers.length > WINDOW_SIZE) {
-    difference = WINDOW_SIZE - evenNumbers.length;
-    numberWindowSize = evenNumbers.splice(0, n);
-  }
-
-  res.send(
-    200,
-    json({
-      windowPrevState: [],
-      windowCurrentState: numberWindowSize,
-      numbers: evenNumbers,
-      avg: calculateAverage(numberWindowSize),
-    })
-  );
-});
-
 // For random numbers
 app.get("/numbers/r", (_, res) => {
   let randNumbers;
   let numberWindowSize;
   axios
-    .get(`${process.env.NUMBERS_URL}` + "rand", axiosConfig)
+    .get(`${NUMBERS_URL}` + "rand", axiosConfig)
     .then((response) => {
       randNumbers = response.data.numbers;
-      console.log("Response:", response.data);
+      numberWindowSize = [...randNumbers];
+
+      if (randNumbers.length > WINDOW_SIZE) {
+        const difference = WINDOW_SIZE - randNumbers.length;
+        numberWindowSize.splice(0, difference);
+      }
+    
+      res.status(200)
+      .send(
+        {
+          windowPrevState: [],
+          windowCurrentState: numberWindowSize,
+          numbers: randNumbers,
+          avg: calculateAverage(numberWindowSize),
+        }
+      )
     })
     .catch((error) => {
       console.error("Error:", error);
     });
-
-  if (randNumbers.length > WINDOW_SIZE) {
-    difference = WINDOW_SIZE - randNumbers.length;
-    numberWindowSize = randNumbers.splice(0, n);
-  }
-
-  res.send(
-    200,
-    json({
-      windowPrevState: [],
-      windowCurrentState: numberWindowSize,
-      numbers: randNumbers,
-      avg: calculateAverage(numberWindowSize),
-    })
-  );
+  
 });
